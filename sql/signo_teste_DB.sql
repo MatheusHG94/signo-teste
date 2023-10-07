2b964111ed89cdbbde954d401577af535116e109
Matheus <mat.hen.gra@gmail.com>
2023-10-07 14:21:04 -0300

    update types and constraints

--- sql/signo_teste_DB.sql
+++ sql/signo_teste_DB.sql
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS pedidos (
     cep VARCHAR(8),
     cidade VARCHAR(255),
     uf VARCHAR(2),
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
     telefone VARCHAR(11) NOT NULL,
     tipo_revistinha ENUM("convite", "lembranca", "convite-lembranca") NOT NULL,
     quantidade INT NOT NULL,
-    atracoes_do_evento VARCHAR(255) NOT NULL,
-    aceita_sugestoes TINYINT ZEROFILL NOT NULL,
+    atracoes_do_evento VARCHAR(510) NOT NULL,
+    aceita_sugestoes TINYINT,
     imagens VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
